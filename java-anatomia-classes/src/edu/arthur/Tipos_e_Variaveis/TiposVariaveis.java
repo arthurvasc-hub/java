2fd107b821b528115417b34683c5ec7ec8329456
arthurvasc <arthurvasc2@gmail.com>
2024-08-21 12:40:27 -0300

    Aprendendo sobre Tipos e Variáveis em JAVA

--- java-anatomia-classes/src/edu/arthur/Tipos_e_Variaveis/TiposVariaveis.java
+++ java-anatomia-classes/src/edu/arthur/Tipos_e_Variaveis/TiposVariaveis.java
@@ -1,8 +1,18 @@
 package edu.arthur.Tipos_e_Variaveis;
 public class TiposVariaveis {
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception {
+        double salarioMinimo = 2500;
+        double bonusSalarial = 0.5; 
 
+        double salarioReal = salarioFinal(salarioMinimo, bonusSalarial);
+        System.out.println(salarioReal);
     }
-    
+    public static double salarioFinal(double salarioMinimo, double bonusSalarial) {
+    return (salarioMinimo * bonusSalarial) + salarioMinimo;
+    }
+
+    short numeroCurto = 1;
+    int numeroNomal = numeroCurto;
+    short numeroNormal2 = (short) numeroNomal; // Error de CASTING
 }
 
