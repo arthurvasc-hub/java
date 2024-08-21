4a7dec18c999ae35601aa263312402456d03bfbf
arthurvasc <arthurvasc2@gmail.com>
2024-08-21 09:24:47 -0300

    Estudos sobre identação em JAVA

---
+++ java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,11 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int medialFinal = 5;
+        if(medialFinal < 6)
+            System.out.println("REPROVADO");
+        else if(medialFinal == 6)
+            System.out.println("PROVA DE MINERVA");
+        else
+            System.out.println("APROVADO");
+    }
+}
