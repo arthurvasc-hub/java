080e98b74b93b7e9305b5d2d543282dc7e76448b
arthurvasc <arthurvasc2@gmail.com>
2024-08-21 00:54:08 -0300

    Anatomias JAVA

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,15 +1,23 @@
 public class MinhaClasse {
     
 public static void main(String[] args) {
-    String primeiroNome = "Arthur";
-    String segundoNome = "Vasconcelos";
-    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
-    System.out.println(nomeCompleto);
+   int year = 1999;
+   int month = 03;
+   int day = 25;
+   String firstName = "Arthur";
+   String secondName = "Vasconcelos";
+   String fullName = fullName(firstName, secondName);
+   String dateOfBirth = dateOfBirth(year, month, day);
+   System.out.println(fullName);
+   System.out.println(dateOfBirth);
 }
 
-public static String nomeCompleto(String primeiroNome, String segundoNomeString) {
-    return primeiroNome.concat(" ").concat(segundoNomeString);
+public static String fullName(String firstName, String secondName) {
+    return firstName.concat(" ").concat(secondName);
 }
 
+public static String dateOfBirth(int year, int month, int day) {
+    return year + "/" + month + "/" + day;
+}
 }
 
