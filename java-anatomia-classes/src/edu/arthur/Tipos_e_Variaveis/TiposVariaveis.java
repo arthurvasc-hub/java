3cdb9b906862d6cbbaf1568dba0bc57a30e9b10d
arthurvasc <arthurvasc2@gmail.com>
2024-08-21 12:56:45 -0300

    Aprendendo sobre CONSTANTES em JAVA

--- java-anatomia-classes/src/edu/arthur/Tipos_e_Variaveis/TiposVariaveis.java
+++ java-anatomia-classes/src/edu/arthur/Tipos_e_Variaveis/TiposVariaveis.java
@@ -1,18 +1,19 @@
 package edu.arthur.Tipos_e_Variaveis;
 public class TiposVariaveis {
     public static void main (String[] args) throws Exception {
-        double salarioMinimo = 2500;
-        double bonusSalarial = 0.5; 
+        
+        final double VALOR_DE_PI = 3.14;
+       
+        System.out.println(VALOR_DE_PI);
 
-        double salarioReal = salarioFinal(salarioMinimo, bonusSalarial);
-        System.out.println(salarioReal);
+
     }
     public static double salarioFinal(double salarioMinimo, double bonusSalarial) {
     return (salarioMinimo * bonusSalarial) + salarioMinimo;
     }
 
-    short numeroCurto = 1;
-    int numeroNomal = numeroCurto;
-    short numeroNormal2 = (short) numeroNomal; // Error de CASTING
+    
+
+
 }
 
