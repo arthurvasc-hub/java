b273b7770f2494571613d57b85442c50fec2d038
arthurvasc <arthurvasc2@gmail.com>
2024-08-21 09:42:00 -0300

    Estudos sobre PACKAGE JAVA

--- java-anatomia-classes/src/edu/arthur/anatomia/BoletimEstudantil.java
+++ java-anatomia-classes/src/edu/arthur/anatomia/BoletimEstudantil.java
@@ -1,3 +1,4 @@
+package edu.arthur.anatomia;
 public class BoletimEstudantil {
     public static void main(String[] args) {
         int medialFinal = 5;
--- java-anatomia-classes/src/edu/arthur/anatomia/MinhaClasse.java
+++ java-anatomia-classes/src/edu/arthur/anatomia/MinhaClasse.java
@@ -1,3 +1,4 @@
+package edu.arthur.anatomia;
 public class MinhaClasse {
     
 public static void main(String[] args) {
