18001fa4ca3ef22a2d7596a131c11f33b626edf7
arthurvasc <arthurvasc2@gmail.com>
2024-08-22 17:09:45 -0300

    Aprendendo sobre Operadores Relacionais em JAVA

--- java-anatomia-classes/src/edu/arthur/Operadores/Operadores.java
+++ java-anatomia-classes/src/edu/arthur/Operadores/Operadores.java
@@ -2,11 +2,21 @@ package edu.arthur.Operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-     int a, b;
+     int a, b, c ;
     a = 6;
-    b = 6;
-    String resultado = a == b ? "Verdadeiro" : "Falso";
-        System.out.println(resultado);
+    b = 1;
+    c = 6;
+    boolean simNao = b == a;
+    System.out.println("Numero 1 é igual ao numero 2?: " + simNao);
+    simNao = c > a;
+    System.out.println("Numero 1 é maior que o numero 2?: " + simNao);
+    simNao = a == c;
+    System.out.println("Numero 1 é igual numero 2?: " + simNao);
+    simNao = a != c;
+    System.out.println("Numero 1 é diferente do numero 2?: " + simNao);
+
+    String resultado = ((c * b) == a) ? "Verdadeiro" : "Falso";
+        System.out.println("c * b é igual a 6?: " + resultado);
     }
     
    
