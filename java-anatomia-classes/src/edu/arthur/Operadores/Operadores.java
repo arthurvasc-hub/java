681c990292e9a6459498fdeb3032cfcfb4ae2699
arthurvasc <arthurvasc2@gmail.com>
2024-08-22 16:48:32 -0300

    Aprendendo sobre Operação Ternária em JAVA

--- java-anatomia-classes/src/edu/arthur/Operadores/Operadores.java
+++ java-anatomia-classes/src/edu/arthur/Operadores/Operadores.java
@@ -2,18 +2,11 @@ package edu.arthur.Operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-     int numero = 5;
-
-     boolean variavel = true;
-     variavel = !variavel;
-    System.out.println(!variavel);
-
-     System.out.println(-- numero);
-     System.out.println(++ numero);
-
-     // numero++ = numero = numero + 1;
-
-
+     int a, b;
+    a = 6;
+    b = 6;
+    String resultado = a == b ? "Verdadeiro" : "Falso";
+        System.out.println(resultado);
     }
     
    
