ff38e786361d704be98eb879c98d32d524800821
arthurvasc <arthurvasc2@gmail.com>
2024-08-27 11:13:16 -0300

    Aprendendo os tipos de comentários em JAVA

---
+++ java-anatomia-classes/src/edu/arthur/Tags/Comentarios.java
@@ -0,0 +1,21 @@
+package edu.arthur.Tags;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        // Comentário em uma única linha.
+
+        /* 
+         * Olá, esse é o Mult Line ->
+         * Hehehe
+         * hehehe
+         */
+        
+        /**
+         * Essas duas estrelinhas acima
+         * é para identificar que eu 
+         * pretendo elaborar um comentário
+         * a nível de documentação.
+         */
+    }
+    
+}
--- java-anatomia-classes/src/edu/arthur/Tags/Tags.java
+++ java-anatomia-classes/src/edu/arthur/Tags/Tags.java
@@ -1,6 +1,24 @@
 package edu.arthur.Tags;
+/** 
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes 
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Arthur Vasconcelos
+ * @version 1.0.0
+ * @since 27/08/2024
+ */
 
-public class Tags {
-
-
-}
+ public class Tags {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado desde método é a soma dos dois numeros
+     */
+    public int somar(int numeroUm, int numeroDois) {
+    return numeroUm + numeroDois;
+    }    
+    }
