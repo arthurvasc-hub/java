356cac9cc0cf7cf7db799586534dd2ecb2603403
arthurvasc <arthurvasc2@gmail.com>
2024-08-28 12:16:41 -0300

    Aprendendo a executar comando através do terminal em JAVA

---
+++ java-anatomia-classes/src/edu/arthur/TerminalArgumentos/Terminal.java
@@ -0,0 +1,7 @@
+package edu.arthur.TerminalArgumentos;
+
+public class Terminal {
+    public static void main(String[] args) {
+        System.out.println("Oi, fui executado pelo Terminal");
+    }
+}
