efe3675d646691df0535df1f7dae8b9cf755c7b6
arthurvasc <arthurvasc2@gmail.com>
2024-08-28 18:52:10 -0300

    Aprendendo sobre a classe scanner em JAVA

--- java-anatomia-classes/src/edu/arthur/TerminalArgumentos/Argumentos.java
+++ java-anatomia-classes/src/edu/arthur/TerminalArgumentos/Argumentos.java
@@ -1,14 +1,27 @@
 package edu.arthur.TerminalArgumentos;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Argumentos {
         public static void main(String[] args) {
-            String nome = args[0];
-            String sobrenome = args[1];
-            int idade = Integer.valueOf(args[2]);
-            double altura = Double.valueOf(args[3]);
-        
-            System.out.println("Olá, me chamo " + nome + " " + sobrenome);
-            System.out.println("Tenho " + idade + " anos");
-            System.out.println("E possuo " + altura + "cm de altura");
+           Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+           
+           System.out.println("Digite seu nome: ");
+           String nome = scanner.next();
+
+           System.out.println("Digite seu Sobrenome: ");
+           String sobrenome = scanner.next();
+
+           System.out.println("Digite sua idade: ");
+           int idade = scanner.nextInt();
+
+           System.out.println("Digite sua altura");
+           double altura = scanner.nextDouble();
+
+
+           System.out.println("Olá, meu nome é: " + nome + " " + sobrenome);
+           System.out.println("Tenho " + idade + " anos de idade");
+           System.out.println("E tenho " + altura + "cm de altura");
         }
 }
