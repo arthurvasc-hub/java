b386f436b2e054d74e5b46fda59d88b43fb906b3
arthurvasc <arthurvasc2@gmail.com>
2024-08-29 16:35:08 -0300

    Aprendendo  condicional Switch/Case em JAVA

---
+++ java-controle-fluxo/Condicionais/src/edu/arthur/switchCase/SistemaMedida.java
@@ -0,0 +1,17 @@
+package edu.arthur.switchCase;
+
+public class SistemaMedida {
+    public static void main(String[] args) {
+        String canal = "G"; // M, T ou G
+        // Não utilizar o "break" póis queremos dar continuidade hierarquica no fluxo.
+        switch (canal) {
+            case "G":
+            System.out.println("5gb de Youtube +");
+            case "M":
+            System.out.println("Whats e Instagram GRATIS +");
+            case "P":
+            System.out.println("100 minutos de ligação");
+                
+        }
+    }
+}
