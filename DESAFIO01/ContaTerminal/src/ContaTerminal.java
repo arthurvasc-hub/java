0938ac908f88bbdcbdea9357e527178af13d139f
arthurvasc <arthurvasc2@gmail.com>
2024-08-29 14:16:58 -0300

    Corrigindo espaçamento

--- DESAFIO01/ContaTerminal/src/ContaTerminal.java
+++ DESAFIO01/ContaTerminal/src/ContaTerminal.java
@@ -18,6 +18,6 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o seu saldo!");
             double saldo = scanner.nextDouble();
         
-        System.out.println("Olá "+ nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo R$" + saldo + " já está disponível para saque.");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo R$" + saldo + " já está disponível para saque.");
     }
 }
