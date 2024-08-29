c5c5c3877272ed52a1786f6124afc114271b713c
arthurvasc <arthurvasc2@gmail.com>
2024-08-29 15:59:06 -0300

    Aprendendo condicionais compostas em JAVA

---
+++ java-controle-fluxo/Condicionais/src/edu/arthur/condicionais/Compostas/ResultadoEscolar.java
@@ -0,0 +1,12 @@
+package edu.arthur.condicionais.Compostas;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 6;
+
+        if(nota >= 7)
+            System.out.println("Você foi aprovado");
+        else
+            System.out.println("Você foi reprovado");
+    }
+}
