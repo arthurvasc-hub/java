1fa0bcd504bc7fda599692fde4018bf88cce41b3
arthurvasc <arthurvasc2@gmail.com>
2024-08-29 15:50:00 -0300

    Aprendendo sobre Condicional Simples em JAVA

---
+++ java-controle-fluxo/Condicionais/src/edu/arthur/condicionais/Simples/CaixaEletronico.java
@@ -0,0 +1,14 @@
+package edu.arthur.condicionais.Simples;
+
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 15.0;
+
+        if(valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
+
+         System.out.println(saldo);
+
+    }
+}
