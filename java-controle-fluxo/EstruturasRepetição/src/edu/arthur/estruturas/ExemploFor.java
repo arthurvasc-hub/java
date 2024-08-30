691e08df93d115d7c4e7a5b6cf7a1649dd9e6146
arthurvasc <arthurvasc2@gmail.com>
2024-08-30 12:27:21 -0300

    Aprendendo sobre laço de iteração FOR em JAVA

---
+++ java-controle-fluxo/EstruturasRepetição/src/edu/arthur/estruturas/ExemploFor.java
@@ -0,0 +1,13 @@
+package edu.arthur.estruturas;
+
+public class ExemploFor {
+        public static void main(String[] args) {
+            
+            for(int carneirinhos = 0; carneirinhos <= 20; carneirinhos++){
+                System.out.println("Contando carneirinhos " + carneirinhos);
+            }
+
+
+
+        }
+}
