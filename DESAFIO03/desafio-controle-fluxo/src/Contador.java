6d67d20e625e1219098e1941be551af79bc46cc1
arthurvasc <arthurvasc2@gmail.com>
2024-09-07 16:47:33 -0300

    Desafio Contador, controle de fluxo em JAVA #100DaysOfCode

--- DESAFIO03/desafio-controle-fluxo/src/Contador.java
+++ DESAFIO03/desafio-controle-fluxo/src/Contador.java
@@ -1,25 +1,35 @@
 import java.util.Scanner;
-
 public class Contador {
+    
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Digite o primeiro numero");
         int parametroUm = scanner.nextInt();
+        System.out.println("Digite o segundo numero");
         int parametroDois = scanner.nextInt();
 
         try {
             //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
-        } catch(? Exception) {
+        } catch(ParametrosInvalidosException e) {
+            e.printStackTrace();
             //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
         }
     }
 
     static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-
-        int contagem = parametroDois - parametroUm;
-        //realizar o for para imprimir os números com base na variável contagem
+             if(parametroUm > parametroDois)
+             throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+             else {
+                int contagem = parametroDois - parametroUm;
+                //realizar o for para imprimir os números com base na variável contagem
+                for(int i = 1; i <= contagem; i++){
+                    System.out.println("Imprimindo o numero: " + i);
+                }
+                
+        }
+        
         
     }
 }
