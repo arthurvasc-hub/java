d0341abb933f7b828fffe679c13a034fd18b36d7
arthurvasc <arthurvasc2@gmail.com>
2024-09-06 21:52:20 -0300

    Case 02/DESAFIO em JAVA

--- DESAFIO02/controle-candidatos/src/Candidatura.java
+++ DESAFIO02/controle-candidatos/src/Candidatura.java
@@ -1,21 +1,37 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.Locale;
+
 
 public class Candidatura {
     public static void main(String[] args) throws Exception {
-        System.out.println("Processo Seletivo!");
-        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-
-        System.out.println("Digite aqui sua pretensão salarial: ");
-        double salarioPretendido = scanner.nextDouble();
-
-        
-        analisarCandidato(salarioPretendido);
-
-        scanner.close();
-
+        selecaoCandidato();
     }
-    static void analisarCandidato(double salarioPretendido){
+
+    static void selecaoCandidato(){
+        String [] candidatos = {"ANA", "BRUNO", "CARLA", "DANIEL", "EVA", "FERNANDO", "GABRIEL", "HELENA", "IGOR", "JULIA", "KARLA", "LEONARDO", "MARIA", "NATALIA", "OLGA", "PAULO", "QUITERIA", "RAFAEL", "SANDRA", "TIAGO"}; 
+        int candidatoAtual = 0;
+        int candidatosSelecionados = 0;
+        double salarioOferecido = 2000.0;
+
+        while (candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+            double salarioPretendido = valorPretendido();
+            String candidato = candidatos[candidatoAtual];
+            System.out.printf("O candidato %s pediu como salario: %.2f%n", candidato, salarioPretendido);
+            if(salarioOferecido >= salarioPretendido){
+                System.out.println("E garantiu a vaga na empresa :)! ");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+}
+
+    /* static void analisarCandidato(double salarioPretendido){
         double salarioOfertado = 2000.0;
 
         if(salarioPretendido > salarioOfertado)
@@ -26,3 +42,4 @@ public class Candidatura {
             System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS.");
     }
 }
+    */
