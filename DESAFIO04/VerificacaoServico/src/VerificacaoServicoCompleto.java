85d492015d45bb65f4a7d47b6096a10d17ba0341
arthurvasc <arthurvasc2@gmail.com>
2024-09-09 19:16:23 -0300

    Desafio 005 em JAVA

---
+++ DESAFIO04/VerificacaoServico/src/VerificacaoServicoCompleto.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class VerificacaoServicoCompleto {
+
+    // Função para verificar se o cliente contratou um combo completo
+    public static String verificarComboCompleto(String[] servicosContratados) {
+        // Variáveis booleanas para verificar a contratação de cada serviço
+        boolean movelContratado = false;
+        boolean bandaLargaContratada = false;
+        boolean tvContratada = false;
+
+        // TODO: Itere sobre os serviços contratados
+        for (String servico : servicosContratados) {
+            switch(servico) {
+                case "movel":
+                movelContratado = true;
+                break;
+                case "banda larga":
+                bandaLargaContratada = true;
+                case "tv":
+                tvContratada = true;
+                break;
+            }
+        }
+        
+
+        // TODO: Verifique se todos os serviços foram contratados
+        if (movelContratado == true & bandaLargaContratada == true & tvContratada == true) {
+            return "Combo Completo";
+        } else {
+            return "Combo Incompleto";
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando ao usuário a lista de serviços contratados
+        System.out.println("Digite aqui os serviços contratados");
+        String input = scanner.nextLine();
+
+        // Convertendo a entrada em uma lista de strings
+        String[] servicosContratados = input.split(",");
+
+        // Verificando se o cliente contratou um combo completo
+        String resultado = verificarComboCompleto(servicosContratados);
+
+        // Exibindo o resultado
+        System.out.println(resultado);
+
+        // Fechando o scanner
+        scanner.close();
+    }
+}
