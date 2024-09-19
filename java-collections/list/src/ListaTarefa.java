e3306c260efdbd63b7ca1073854cba077e928813
arthurvasc <arthurvasc2@gmail.com>
2024-09-19 16:42:51 -0300

    Aprendendo sobre Collections - List

---
+++ java-collections/list/src/ListaTarefa.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa { 
+    private List<Tarefa> listaDeTarefas;
+
+    public ListaTarefa(){
+        this.listaDeTarefas = new ArrayList<>();
+    }
+
+    public List<Tarefa> get(){
+        return listaDeTarefas;
+    }
+
+
+    public void adicionarTarefa(String descricao){
+        listaDeTarefas.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> listaRemoverTarefas = new ArrayList<>();
+            for(Tarefa t : listaDeTarefas){
+                if(t.getDescricao().equalsIgnoreCase(descricao)) {
+                    listaRemoverTarefas.add(t);
+                }
+        listaDeTarefas.removeAll(listaRemoverTarefas);
+            }
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return listaDeTarefas.size();
+    }
+
+    public void obterDescricaoTarefas(){
+        System.out.println(listaDeTarefas);
+    }
+}
